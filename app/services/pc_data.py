f2ba92b27f41f6b56bcdadbf587024f13e1efc81
Vladimir <volosnikovvladimir69@gmail.com>
2025-01-11 21:50:30 +0500

    Add docstrings; make the field __interval private; add a check to the delay setter that interval is a number.;

--- app/services/pc_data.py
+++ app/services/pc_data.py
@@ -1,4 +1,6 @@
-from typing import Tuple, Dict
+"""The module responsible for receiving information about the CPU, RAM, and ROM."""
+
+from typing import Dict, Tuple
 
 import psutil
 
@@ -6,31 +8,53 @@ from app.utils.human_readable import readable_memory
 
 
 class PCData(object):
+    """
+    A class for getting information about the PC load on the CPU, RAM, and ROM.
+
+    Args:
+        interval (float) - The time interval during which the CPU usage is measured.
+        It cannot be less than 1 second.
+    """
+
     def __init__(self, interval: float = 1):
-        self.interval = interval
-        if self.interval < 1:
-            self.interval = 1
+        """
+        Initialize the class.
+
+        :param interval: the time interval during which the CPU usage is measured.
+        It cannot be less than 1 second.
+        """
+        self.__interval = interval
+        if self.__interval < 1:
+            self.__interval = 1
 
     @property
     def delay(self) -> float:
-        return self.interval
+        """Get self.__interval."""
+        return self.__interval
 
     @delay.setter
     def delay(self, new_interval: float) -> None:
-        self.interval = new_interval
-        if self.interval < 1:
-            self.interval = 1
+        """Set self.__interval."""
+        if isinstance(new_interval, float | int):
+            self.__interval = new_interval
+            if self.__interval < 1:
+                self.__interval = 1
+        else:
+            self.__interval = 1
 
-    def __cpu(self, interval: float = 1) -> float:
-        return psutil.cpu_percent(self.interval)
+    def __cpu(self) -> float:
+        """Return the CPU usage as a percentage."""
+        return psutil.cpu_percent(self.__interval)
 
     @classmethod
     def __ram(cls) -> Tuple[int, int]:
+        """Return free and total ram."""
         memory: psutil.svmem = psutil.virtual_memory()
         return memory.available, memory.total
 
     @classmethod
     def __rom(cls) -> Tuple[int, int]:
+        """Return free and total rom."""
         free: int = 0
         total: int = 0
 
@@ -43,6 +67,7 @@ class PCData(object):
 
     @property
     def data_dict(self) -> Dict[str, str]:
+        """Return information about CPU, RAM, and ROM usage as a dictionary."""
         ram_free, ram_total = self.__ram()
         rom_free, rom_total = self.__rom()
         return {
