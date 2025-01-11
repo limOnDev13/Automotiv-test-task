de45b75cbfde7e1658c4afc0127c1bbe7bc7742a
Vladimir <volosnikovvladimir69@gmail.com>
2025-01-11 17:51:21 +0500

    Add method __init__; add getter and setter delay; make the __cpu, __ram, and __rom methods private; add getter data_dict

--- app/services/pc_data.py
+++ app/services/pc_data.py
@@ -1,28 +1,52 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 import psutil
 
+from app.utils.human_readable import readable_memory
+
 
 class PCData(object):
-    @classmethod
-    def cpu(cls, interval: float = 1) -> float:
-        if interval < 1:
-            interval = 1
-        return psutil.cpu_percent(interval)
+    def __init__(self, interval: float = 1):
+        self.interval = interval
+        if self.interval < 1:
+            self.interval = 1
+
+    @property
+    def delay(self) -> float:
+        return self.interval
+
+    @delay.setter
+    def delay(self, new_interval: float) -> None:
+        self.interval = new_interval
+        if self.interval < 1:
+            self.interval = 1
+
+    def __cpu(self, interval: float = 1) -> float:
+        return psutil.cpu_percent(self.interval)
 
     @classmethod
-    def ram(cls) -> Tuple[int, int]:
+    def __ram(cls) -> Tuple[int, int]:
         memory: psutil.svmem = psutil.virtual_memory()
         return memory.available, memory.total
 
     @classmethod
-    def rom(cls) -> Tuple[int, int]:
-        used: int = 0
+    def __rom(cls) -> Tuple[int, int]:
+        free: int = 0
         total: int = 0
 
         for disk in psutil.disk_partitions(all=False):
             disk_usage = psutil.disk_usage(disk.mountpoint)
-            used += disk_usage.used
+            free += disk_usage.free
             total += disk_usage.total
 
-        return used, total
+        return free, total
+
+    @property
+    def data_dict(self) -> Dict[str, str]:
+        ram_free, ram_total = self.__ram()
+        rom_free, rom_total = self.__rom()
+        return {
+            "CPU": f"{self.__cpu()}",
+            "RAM": f"{readable_memory(ram_free)} / {readable_memory(ram_total)}",
+            "ROM": f"{readable_memory(rom_free)} / {readable_memory(rom_total)}",
+        }
