f7cb02553cd1cefd924616dc3768263c4c60e171
Vladimir <volosnikovvladimir69@gmail.com>
2025-01-12 22:34:28 +0500

    Delete delay from test

--- app/tests/tests_services/test_pc_data.py
+++ app/tests/tests_services/test_pc_data.py
@@ -6,19 +6,6 @@ from app.services.pc_data import PCData
 def test_class_pcdata() -> None:
     """Test the class PCData."""
     pc_stat: PCData = PCData()
-    assert pc_stat.delay == 1
-
-    pc_stat.delay = 0.5
-    assert pc_stat.delay == 1
-
-    pc_stat.delay = "3.3"
-    assert pc_stat.delay == 1
-
-    pc_stat.delay = 3.3
-    assert pc_stat.delay == 3.3
-
-    pc_stat.delay = "asdasd"
-    assert pc_stat.delay == 1
 
     data = pc_stat.data_dict
     assert "CPU" in data
