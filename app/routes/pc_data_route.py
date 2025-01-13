76289a417c6393c1b7d26e8e991b9f3231667603
Vladimir <volosnikovvladimir69@gmail.com>
2025-01-13 11:14:57 +0500

    Add the GET endpoint / to get the home page

--- app/routes/pc_data_route.py
+++ app/routes/pc_data_route.py
@@ -1,13 +1,22 @@
 """The module responsible for transmitting PC data via websockets."""
 
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from app.config.app_config import Config
 from app.services.history import HistoryManager
 
 router: APIRouter = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 DELAY: int = Config().delay
+
+
+@router.get("/", response_class=HTMLResponse)
+async def main_page(request: Request):
+    """Get the main page."""
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @router.websocket("/ws")
@@ -19,4 +28,5 @@ async def send_stats(websocket: WebSocket):
         websocket=websocket,
         interval=DELAY,
     )
+
     await history.run()
